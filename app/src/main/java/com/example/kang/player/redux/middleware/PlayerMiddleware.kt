5cab2729aa05d7e1b99bc3c7c4a15f70f4e2dd52
kang <kjxstar@gmail.com>
2017-03-17 16:06:08 +0800

    add player middleware

---
+++ app/src/main/java/com/example/kang/player/redux/middleware/PlayerMiddleware.kt
@@ -0,0 +1,20 @@
+package com.example.kang.player.redux.middleware
+
+import com.example.kang.player.model.PlayerState
+import com.example.redux.IDispatcher
+import com.example.redux.Middleware
+import com.example.redux.Store
+
+
+/**
+ * Created by kang on 17-3-13.
+ */
+class PlayerMiddleware : Middleware<PlayerState> {
+    override fun create(store: Store<PlayerState>, nextDispatcher: IDispatcher): IDispatcher {
+        return IDispatcher { action ->
+            when (action.type) {
+                else -> nextDispatcher.dispatch(action)
+            }
+        }
+    }
+}
