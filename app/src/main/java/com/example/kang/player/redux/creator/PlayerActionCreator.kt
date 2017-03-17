642b4e8c8205c0266848358a475c0fc8d569680b
kang <kjxstar@gmail.com>
2017-03-17 17:58:42 +0800

    add music switch action

--- app/src/main/java/com/example/kang/player/redux/creator/PlayerActionCreator.kt
+++ app/src/main/java/com/example/kang/player/redux/creator/PlayerActionCreator.kt
@@ -13,4 +13,5 @@ object PlayerActionCreator : Action<Any>("", "") {
     fun prevSong() = Action(Actions.ACTION_PREVIOUS_SONG, Any())
     fun pause() = Action(Actions.ACTION_PAUSE_SONG, Any())
     fun play() = Action(Actions.ACTION_PLAY_SONG,Any())
+    fun switch(uri: Uri) = Action(Actions.ACTION_SWITCH_SONG, uri)
 }
