23e90c19adce989bbab46090af040076a07d65f3
kang <kjxstar@gmail.com>
2017-03-17 15:29:45 +0800

    add field to player state

--- app/src/main/java/com/example/kang/player/model/PlayerState.kt
+++ app/src/main/java/com/example/kang/player/model/PlayerState.kt
@@ -4,9 +4,12 @@ package com.example.kang.player.model
  * Created by kang on 17-3-17.
  */
 data class PlayerState(
+        var playlist: MutableCollection<Music>,
         var index: Int,
         var playMode: PlayMode
 )
+
+data class Music(val name: String, val duration: Long)
 
 enum class PlayMode {
     SHUFFLE,
