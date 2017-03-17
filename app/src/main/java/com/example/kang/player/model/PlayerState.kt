213bb2ab108275635047017c8f776da7a7aefecd
kang <kjxstar@gmail.com>
2017-03-17 16:05:43 +0800

    data class deep copy

--- app/src/main/java/com/example/kang/player/model/PlayerState.kt
+++ app/src/main/java/com/example/kang/player/model/PlayerState.kt
@@ -1,15 +1,31 @@
 package com.example.kang.player.model
 
+import java.util.*
+
 /**
  * Created by kang on 17-3-17.
  */
 data class PlayerState(
         var playlist: MutableCollection<Music>,
         var index: Int,
-        var playMode: PlayMode
-)
+        var playMode: PlayMode,
+        var playing: Boolean) : Cloneable {
 
-data class Music(val name: String, val duration: Long)
+    override fun clone(): PlayerState {
+        val list = ArrayList<Music>()
+        playlist.forEach {
+            list.add(it.copy())
+        }
+
+        return PlayerState(list, index, playMode, playing)
+    }
+}
+
+data class Music(val name: String, val duration: Long) : Cloneable {
+    override fun clone(): Music {
+        return Music(name, duration)
+    }
+}
 
 enum class PlayMode {
     SHUFFLE,
