c8c1c68bba88e737c8bbce0e048d7ef317a49514
kang <kjxstar@gmail.com>
2017-03-17 16:05:28 +0800

    add play/pause case

--- app/src/main/java/com/example/kang/player/redux/reducer/PlayerReducer.kt
+++ app/src/main/java/com/example/kang/player/redux/reducer/PlayerReducer.kt
@@ -1,6 +1,7 @@
 package com.example.kang.player.redux.reducer
 
 import com.example.kang.player.model.PlayerState
+import com.example.kang.player.redux.Actions
 import com.example.redux.Action
 import com.example.redux.IReducer
 
@@ -9,6 +10,23 @@ import com.example.redux.IReducer
  */
 class PlayerReducer : IReducer<PlayerState> {
     override fun reduce(state: PlayerState, action: Action<Any>): PlayerState {
+        when (action.type) {
+            Actions.ACTION_PREVIOUS_SONG -> {
+
+            }
+            Actions.ACTION_NEXT_SONG -> {
+
+            }
+            Actions.ACTION_PAUSE_SONG -> return state.copy().apply {
+                playing = false
+            }
+            Actions.ACTION_SEEK_SONG -> {
+
+            }
+            Actions.ACTION_PLAY_SONG -> return state.copy().apply {
+                playing = true
+            }
+        }
         return state
     }
 }
