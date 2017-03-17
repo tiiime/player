11a1e59b06c749a02dc9bd7af84d4250a601cbd4
kang <kjxstar@gmail.com>
2017-03-17 13:42:20 +0800

    new hsm

--- app/src/main/java/com/example/kang/player/logic/MusicStateMachine.kt
+++ app/src/main/java/com/example/kang/player/logic/MusicStateMachine.kt
@@ -2,13 +2,17 @@ package com.example.kang.player.logic
 
 import com.example.kang.player.logic.state.*
 import com.example.kang.player.util.StateMachine
+import com.google.android.exoplayer2.ExoPlayer
+
 
 /**
  * Created by kang on 17-3-16.
  *
  */
-class MusicStateMachine(name: String) : StateMachine(name) {
+class MusicStateMachine(name: String, val player: ExoPlayer) : StateMachine(name) {
+
     companion object {
+        internal val CMD_INIT = 0x00
         internal val CMD_PLAY = 0x01
         internal val CMD_SEEK = 0x02
         internal val CMD_RESET = 0x03
@@ -17,38 +21,40 @@ class MusicStateMachine(name: String) : StateMachine(name) {
         internal val CMD_SWITCH = 0x06
     }
 
-    val seekState = SeekState()
-    val pauseState = PauseState()
-    val resetState = ResetInfoState()
-    val bufferState = BufferState()
-    val switchState = SwitchSongState()
-    val playingState = PlayingState()
+    private val initState = InitState(player)
+    private val seekState = SeekState(player)
+    private val pauseState = PauseState(player)
+    private val resetState = ResetInfoState(player)
+    private val bufferState = BufferState(player)
+    private val switchState = SwitchSongState(player)
+    private val playingState = PlayingState(player)
 
     /**
      *
-     *                       +---------+
-     *                       |  PAUSE  |
-     *                       +---------+
-     *                       /         \
-     *           +----------+           +---------+
-     *           |   SEEK   |           |  RESET  |
-     *           +----------+           +---------+
-     *                |                      |
-     *           +----------+           +---------+
-     *           |  BUFFER  |           |  SWITCH |
-     *           +----------+           +---------+
-     *                |
-     *           +----------+
-     *           | PLAYING  |
-     *           +----------+
+     *                      +---------+
+     *                      |  INIT   |
+     *                      +---------+
+     *                      /         \
+     *           +----------+         +---------+
+     *           |  BUFFER  |         |  PAUSE  |
+     *           +----------+         +---------+
+     *                |                  /   \
+     *           +----------+  +---------+   +---------+
+     *           |   PLAY   |  |  RESET  |   |   SEEK  |
+     *           +----------+  +---------+   +---------+
+     *                              |
+     *                         +---------+
+     *                         |  SWITCH |
+     *                         +---------+
      */
     init {
-        addState(pauseState, null)
-            addState(seekState, pauseState)
-                addState(bufferState, seekState)
-                    addState(playingState, bufferState)
-            addState(resetState, pauseState)
-                addState(switchState, resetState)
+        addState(initState, null)
+            addState(bufferState, initState)
+                addState(playingState, bufferState)
+            addState(pauseState, initState)
+                addState(seekState, pauseState)
+                addState(resetState, pauseState)
+                    addState(switchState, resetState)
 
         setInitialState(pauseState)
     }
