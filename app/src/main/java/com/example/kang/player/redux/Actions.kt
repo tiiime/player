48472542135a95a69d39e49875dda8f6d36a4523
kang <kjxstar@gmail.com>
2017-03-17 09:58:16 +0800

    convert java to kt

---
+++ app/src/main/java/com/example/kang/player/redux/Actions.kt
@@ -0,0 +1,12 @@
+package com.example.kang.player.redux
+
+/**
+ * Created by kang on 17-3-16.
+ */
+object Actions {
+    val ACTION_PREVIOUS_SONG = "ACTION_PREVIOUS_SONG"
+    val ACTION_NEXT_SONG = "ACTION_NEXT_SONG"
+    val ACTION_PAUSE_SONG = "ACTION_PAUSE_SONG"
+    val ACTION_PLAY_SONG = "ACTION_PLAY_SONG"
+    val ACTION_SEEK_SONG = "ACTION_SEEK_SONG"
+}
